1d9f4c4f72de5fd0ce1de599bfa43d19d5acdb82
Mohamed AFSSAS <iireco20@gmail.com>
2023-12-29 09:51:29 +0000

    create auth conroller api and settings all method you need

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    # Register method
+    public function register()
+    {
+    }
+
+    # Login method
+    public function login()
+    {
+    }
+
+    # Profile method
+    public function profile()
+    {
+    }
+
+    # Refresh token method
+    public function refreshToken()
+    {
+    }
+
+    # Logout method
+    public function logout()
+    {
+    }
+}
